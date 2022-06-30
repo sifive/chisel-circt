6326ff546924bdf6aca9cf3b573c7d4269270abe
Megan Wachs <megan@sifive.com>
2022-06-30 10:46:35 -0700

    scalafmt

--- src/main/scala/circt/stage/phases/CIRCT.scala
+++ src/main/scala/circt/stage/phases/CIRCT.scala
@@ -6,7 +6,16 @@ import circt.Implicits.BooleanImplicits
 import circt.stage.{CIRCTOptions, CIRCTTarget, EmittedMLIR}
 
 import firrtl.{AnnotationSeq, EmittedVerilogCircuit, EmittedVerilogCircuitAnnotation}
-import firrtl.options.{CustomFileEmission, Dependency, OptionsException, OutputAnnotationFileAnnotation, Phase, StageError, StageOptions, StageUtils}
+import firrtl.options.{
+  CustomFileEmission,
+  Dependency,
+  OptionsException,
+  OutputAnnotationFileAnnotation,
+  Phase,
+  StageError,
+  StageOptions,
+  StageUtils
+}
 import firrtl.options.phases.WriteOutputAnnotations
 import firrtl.options.Viewer.view
 import firrtl.stage.{FirrtlOptions, RunFirrtlTransformAnnotation}
@@ -37,66 +46,66 @@ class CIRCT extends Phase {
 
   import Helpers._
 
-  override def prerequisites          = Seq(
+  override def prerequisites = Seq(
     Dependency[firrtl.stage.phases.AddDefaults],
     Dependency[firrtl.stage.phases.AddImplicitEmitter],
-    Dependency[firrtl.stage.phases.AddImplicitOutputFile],
+    Dependency[firrtl.stage.phases.AddImplicitOutputFile]
   )
-  override def optionalPrerequisites  = Seq.empty
+  override def optionalPrerequisites = Seq.empty
   override def optionalPrerequisiteOf = Seq.empty
   override def invalidates(a: Phase) = false
 
   override def transform(annotations: AnnotationSeq): AnnotationSeq = {
-    val circtOptions  = view[CIRCTOptions](annotations)
+    val circtOptions = view[CIRCTOptions](annotations)
     val firrtlOptions = view[FirrtlOptions](annotations)
-    val stageOptions  = view[StageOptions](annotations)
+    val stageOptions = view[StageOptions](annotations)
 
     var blackbox, inferReadWrite = false
-    var dedup, imcp              = true
-    var logLevel                 = _root_.logger.LogLevel.None
+    var dedup, imcp = true
+    var logLevel = _root_.logger.LogLevel.None
 
     var split = false
 
     val annotationsx: AnnotationSeq = annotations.flatMap {
-      case a: CustomFileEmission                             => {
+      case a: CustomFileEmission => {
         val filename = a.filename(annotations)
         a.replacements(filename)
       }
-      case _: firrtl.EmitCircuitAnnotation                   => Nil
-      case _: firrtl.EmitAllModulesAnnotation                => {
+      case _: firrtl.EmitCircuitAnnotation => Nil
+      case _: firrtl.EmitAllModulesAnnotation => {
         split = true
         Nil
       }
-      case RunFirrtlTransformAnnotation(transform)           =>
+      case RunFirrtlTransformAnnotation(transform) =>
         transform match {
           /* Inlining/Flattening happen by default, so these can be dropped. */
           case _: firrtl.passes.InlineInstances | _: firrtl.transforms.Flatten => Nil
           /* ReplSeqMem is converted to a firtool option */
-          case _: firrtl.passes.memlib.ReplSeqMem                              =>
+          case _: firrtl.passes.memlib.ReplSeqMem =>
             blackbox = true
             Nil
           /* Any emitters should not be passed to firtool. */
-          case _: firrtl.Emitter                                               => Nil
+          case _: firrtl.Emitter => Nil
           /* Default case: leave the annotation around and let firtool warn about it. */
-          case _                                                               => Nil
+          case _ => Nil
         }
-      case firrtl.passes.memlib.InferReadWriteAnnotation     =>
+      case firrtl.passes.memlib.InferReadWriteAnnotation =>
         inferReadWrite = true
         Nil
-      case _: firrtl.transforms.NoDedupAnnotation            =>
+      case _: firrtl.transforms.NoDedupAnnotation =>
         dedup = false
         Nil
       case firrtl.transforms.NoConstantPropagationAnnotation =>
         imcp = false
         Nil
-      case anno: _root_.logger.LogLevelAnnotation            =>
-        logLevel = anno.globalLogLevel 
+      case anno: _root_.logger.LogLevelAnnotation =>
+        logLevel = anno.globalLogLevel
         Nil
       /* The following can be dropped. */
-      case _: firrtl.transforms.CombinationalPath            => Nil
-      case _: _root_.logger.ClassLogLevelAnnotation          => Nil
+      case _: firrtl.transforms.CombinationalPath   => Nil
+      case _: _root_.logger.ClassLogLevelAnnotation => Nil
       /* Default case: leave the annotation around and let firtool warn about it. */
-      case a                                              => Seq(a)
+      case a => Seq(a)
     }
 
     /* Filter the annotations to only those things which CIRCT should see. */
@@ -134,11 +143,11 @@ class CIRCT extends Phase {
           case (Some(CIRCTTarget.Verilog), false)       => None
           case (Some(CIRCTTarget.SystemVerilog), true)  => Seq("--split-verilog", s"-o=${stageOptions.targetDir}")
           case (Some(CIRCTTarget.SystemVerilog), false) => None
-          case (None, _)                                =>
+          case (None, _) =>
             throw new Exception(
               "No 'circtOptions.target' specified. This should be impossible if dependencies are satisfied!"
             )
-          case _                                        =>
+          case _ =>
             throw new Exception(
               s"Invalid combination of circtOptions.target ${circtOptions.target} and split ${split}"
             )
@@ -151,32 +160,33 @@ class CIRCT extends Phase {
       val stderrStream = new java.io.ByteArrayOutputStream
       val stdoutWriter = new java.io.PrintWriter(stdoutStream)
       val stderrWriter = new java.io.PrintWriter(stderrStream)
-      val exitValue    = (cmd #< new java.io.ByteArrayInputStream(input.getBytes)).!(ProcessLogger(stdoutWriter.println, stderrWriter.println))
+      val exitValue = (cmd #< new java.io.ByteArrayInputStream(input.getBytes))
+        .!(ProcessLogger(stdoutWriter.println, stderrWriter.println))
       stdoutWriter.close()
       stderrWriter.close()
-      val result       = stdoutStream.toString
+      val result = stdoutStream.toString
       logger.info(result)
-      val errors       = stderrStream.toString
+      val errors = stderrStream.toString
       if (exitValue != 0) {
         //logger.error(errors)
         StageUtils.dramaticError(s"${binary} failed.\nExitCode:\n${exitValue}\nSTDOUT:\n${result}\nSTDERR:\n${errors}")
         throw new StageError()
       }
-      if (split) { 
+      if (split) {
         logger.info(result)
         Nil
-      } else {// if split it has already been written out to the file system stdout not necessary.
-        val outputFileName: String                   = stageOptions.getBuildFileName(firrtlOptions.outputFileName.get)
+      } else { // if split it has already been written out to the file system stdout not necessary.
+        val outputFileName: String = stageOptions.getBuildFileName(firrtlOptions.outputFileName.get)
         circtOptions.target match {
-          case Some(CIRCTTarget.FIRRTL)        =>
+          case Some(CIRCTTarget.FIRRTL) =>
             Seq(EmittedMLIR(outputFileName, result, Some(".fir.mlir")))
-          case Some(CIRCTTarget.HW)            =>
+          case Some(CIRCTTarget.HW) =>
             Seq(EmittedMLIR(outputFileName, result, Some(".hw.mlir")))
-          case Some(CIRCTTarget.Verilog)       =>
+          case Some(CIRCTTarget.Verilog) =>
             Seq(EmittedVerilogCircuitAnnotation(EmittedVerilogCircuit(outputFileName, result, ".v")))
           case Some(CIRCTTarget.SystemVerilog) =>
             Seq(EmittedVerilogCircuitAnnotation(EmittedVerilogCircuit(outputFileName, result, ".sv")))
-          case None                            =>
+          case None =>
             throw new Exception(
               "No 'circtOptions.target' specified. This should be impossible if dependencies are satisfied!"
             )
