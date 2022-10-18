0dbaa496b0507bf5d80d5ef3394f46118fc363bc
Schuyler Eldridge <schuyler.eldridge@sifive.com>
2022-10-17 22:39:03 -0400

    Version bump for Trace API support
    Signed-off-by: Schuyler Eldridge <schuyler.eldridge@sifive.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.7"
-  private val chiselVersion = "3.5.4"
+  private val chiselVersion = "3.5-SNAPSHOT"
   lazy val chisel3 = "edu.berkeley.cs" %% "chisel3" % chiselVersion
   lazy val chiselCompilerPlugin = "edu.berkeley.cs" %% "chisel3-plugin" % chiselVersion
 }
