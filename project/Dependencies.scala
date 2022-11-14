01d41caeb0a7278372e06b5681a22784014c3420
Carlos de Paula <me@carlosedp.com>
2022-11-14 14:55:57 -0300

    Use latest stable version of Chisel

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.2.14"
-  private val chiselVersion = "3.6-SNAPSHOT"
+  private val chiselVersion = "3.5.5"
   lazy val chisel3 = "edu.berkeley.cs" %% "chisel3" % chiselVersion
   lazy val chiselCompilerPlugin = "edu.berkeley.cs" %% "chisel3-plugin" % chiselVersion
 }
